836732a2d93ab13b4b3b774cc2ffd3769314c9a7
Lee Lup Yuen <luppy@appkaki.com>
2023-06-02 08:14:23 +0800

    Add termux sample

--- termux/hello_world.zig
+++ termux/hello_world.zig
@@ -11,7 +11,7 @@ pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
     // Create a connection to the plugin
-    var conn: c.tgui_connection = undefined;
+    var conn = c.tgui_connection{};
     if (c.tgui_connection_create(&conn) != 0) {
         std.debug.print("a", .{}); ////
         @panic("Failed to create Termux GUI Connection");
