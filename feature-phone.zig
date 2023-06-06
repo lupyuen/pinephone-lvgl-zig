432505e051bb296a8dd7b4efb79231121f934838
Lee Lup Yuen <luppy@appkaki.com>
2023-06-06 15:25:09 +0800

    Clean up

--- feature-phone.zig
+++ feature-phone.zig
@@ -113,28 +113,40 @@ fn createDisplayLabel(cont: *c.lv_obj_t) !void {
 /// Create the Call and Cancel Buttons
 /// https://docs.lvgl.io/8.3/examples.html#simple-buttons
 fn createCallButtons(cont: *c.lv_obj_t) !void {
+    // For each Button: Call and Connect...
     for (call_labels) |text| {
+        // Create a Button of 250 x 100 pixels
         const btn = c.lv_btn_create(cont);
         c.lv_obj_set_size(btn, 250, 100);
-        _ = c.lv_obj_add_event_cb(btn, eventHandler, c.LV_EVENT_ALL, @intToPtr(*anyopaque, @ptrToInt(text.ptr)));
 
+        // Center the Button Label: Call or Cancel
         const label = c.lv_label_create(btn);
         c.lv_label_set_text(label, text.ptr);
         c.lv_obj_center(label);
+
+        // Set the Event Callback Function and Callback Data for the Button
+        const data = @intToPtr(*anyopaque, @ptrToInt(text.ptr));
+        _ = c.lv_obj_add_event_cb(btn, eventHandler, c.LV_EVENT_ALL, data);
     }
 }
 
 /// Create the Digit Buttons
 /// https://docs.lvgl.io/8.3/examples.html#simple-buttons
 fn createDigitButtons(cont: *c.lv_obj_t) !void {
+    // For each Digit Button...
     for (digit_labels) |text| {
+        // Create a Button of 150 x 120 pixels
         const btn = c.lv_btn_create(cont);
         c.lv_obj_set_size(btn, 150, 120);
-        _ = c.lv_obj_add_event_cb(btn, eventHandler, c.LV_EVENT_ALL, @intToPtr(*anyopaque, @ptrToInt(text.ptr)));
 
+        // Center the Button Label
         const label = c.lv_label_create(btn);
         c.lv_label_set_text(label, text.ptr);
         c.lv_obj_center(label);
+
+        // Set the Event Callback Function and Callback Data for the Button
+        const data = @intToPtr(*anyopaque, @ptrToInt(text.ptr));
+        _ = c.lv_obj_add_event_cb(btn, eventHandler, c.LV_EVENT_ALL, data);
     }
 }
 
